656def177ef69f58687211fc9db0b93d7bd035db
Guanqiong Shi <Sherry-Shi@Guanqiongs-MacBook-Pro.local>
2015-09-15 21:05:35 -0700

    first commit

---
+++ src/rpc/RecommendRestaurants.java
@@ -0,0 +1,96 @@
+package rpc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import db.DBConnection;
+
+/**
+ * Servlet implementation class RecommendRestaurants
+ */
+@WebServlet("/RecommendRestaurants")
+public class RecommendRestaurants extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RecommendRestaurants() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("application/json");
+		response.addHeader("Access-Control-Allow-Origin", "*");
+		JSONObject obj = new JSONObject();
+		List<String> buf = new ArrayList<String>();
+		buf.add("panda express");
+		buf.add("hong kong express");
+		buf.add("chinatown express");
+		
+		try {
+			obj.append("restaurants", buf);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		PrintWriter out = response.getWriter();
+		out.print(obj);
+		out.flush();
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	private static final DBConnection connection = new DBConnection();
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+ 	 StringBuffer jb = new StringBuffer();
+ 	 String line = null;
+ 	 try {
+ 		 BufferedReader reader = request.getReader();
+ 		 while ((line = reader.readLine()) != null) {
+ 			 jb.append(line);
+ 		 }
+ 		 reader.close();
+ 	 } catch (Exception e) { /* report an error */
+ 	 }
+
+ 	 try {
+ 		 JSONObject input = new JSONObject(jb.toString());
+ 		 JSONArray array = null;
+ 		 if (input.has("user_id")) {
+ 			 String user_id = (String) input.get("user_id");
+ 			 array = connection.RecommendRestaurants(user_id);
+ 		 }
+ 		 response.setContentType("application/json");
+ 		 response.addHeader("Access-Control-Allow-Origin", "*");
+ 		 PrintWriter out = response.getWriter();
+ 		 out.print(array);
+ 		 out.flush();
+ 		 out.close();
+ 	 } catch (JSONException e) {
+ 		 e.printStackTrace();
+ 	 }    
+  }
+
+}
